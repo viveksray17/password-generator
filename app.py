bf7bab3b4e7893007199b939f8947484638ed22f
Vivek Sundaray <viveksundaray123@outlook.com>
2021-11-27 15:39:43 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import random
+import string
+
+
+def generate_password(length):
+    lowercasealphabets = list(string.ascii_lowercase)
+    uppercasealphabets = list(string.ascii_uppercase)
+    numbers = list(range(9))
+    password = []
+    for i in range(length):
+        choices = ["lowercasestrings", "uppercasestrings", "numbers"]
+        choice = random.choice(choices)
+        if choice == "lowercasestrings":
+            alpha1 = random.choice(lowercasealphabets)
+            password.append(alpha1)
+        elif choice == "uppercasestrings":
+            alpha2 = random.choice(uppercasealphabets)
+            password.append(alpha2)
+        elif choice == "numbers":
+            num = random.choice(numbers)
+            password.append(num)
+    return ''.join(str(v) for v in password)
+
+
+if __name__ == "__main__":
+    get_length = int(input("enter the length: "))
+    print(generate_password(length=get_length))
